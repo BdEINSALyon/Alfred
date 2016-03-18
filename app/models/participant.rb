da8200541d05af438538d32188b1b35a5c60aab9
Philippe Vienne <philippe.vienne@insa-lyon.fr>
2016-03-18 17:15:46 +0100

    Setup active admin

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,3 +1,13 @@
 class Participant < ActiveRecord::Base
   belongs_to :payment_method
+
+  def label
+    "#{last_name.upcase} #{first_name}"
+  end
+
+  rails_admin do
+    object_label_method do
+      :label
+    end
+  end
 end
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,12 +1,20 @@
 RailsAdmin.config do |config|
 
+  ActiveRecord::Base.descendants.each do |imodel|
+    config.model "#{imodel.name}" do
+      list do
+        exclude_fields :created_at, :updated_at
+      end
+    end
+  end
+
   ### Popular gems integration
 
   ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
+  config.authenticate_with do
+     warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
 
   ## == Cancan ==
   # config.authorize_with :cancan
@@ -23,12 +31,10 @@ RailsAdmin.config do |config|
     dashboard                     # mandatory
     index                         # mandatory
     new
-    export
     bulk_delete
     show
     edit
     delete
-    show_in_app
 
     ## With an audit adapter, you can add:
     # history_index
