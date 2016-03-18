2d988bd5650119e83a314f8ecd61478765b697d5
Philippe Vienne <philippe.vienne@insa-lyon.fr>
2016-03-18 16:22:05 +0100

    Add data into the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+User.create!(email: 'user@example.com', password: 'password', password_confirmation: 'password')
+PaymentMethod.create(name: 'CB')
+PaymentMethod.create(name: 'CHQ')
+PaymentMethod.create(name: 'CHQ')
