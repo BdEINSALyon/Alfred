f4c7bcca78b0bb713b27ac5e1adfb717140d33ca
Philippe Vienne <philippe.vienne@insa-lyon.fr>
2016-03-31 21:10:32 +0200

    Get data from YurPlan

--- app/controllers/yurplan_link_controller.rb
+++ app/controllers/yurplan_link_controller.rb
@@ -7,13 +7,27 @@ class YurplanLinkController < ApplicationController
   end
 
   def load
-    @yurplan = Yurplan.find y_id
-
+    @yurplan = Yurplan.find y_id[:id]
+    @token = HTTParty.post('https://api.yurplan.com/v1/token',
+                       :body => {
+                           :client_id => @yurplan.client_id,
+                           :client_secret => @yurplan.client_secret,
+                           :grant_type => 'client_credentials',
+                           :scope => 'pro',
+                       })['results']['access_token']
+    # noinspection RubyStringKeysInHashInspection
+    HTTParty.get("https://api.yurplan.com/v1/events/#{@yurplan.event_id}/tickets?range=0-999999999",
+                 headers:{'Authorization' => "Bearer #{@token}"})['results'].each do |t|
+      Participant.find_or_create_by ticket_code: t['token'] do |p|
+        p.first_name = t['first_name']
+        p.last_name = t['last_name']
+      end
+    end
   end
 
   private
 
   def y_id
-    params.require(:id)
+    params.require(:yurplan).permit(:id)
   end
 end
