d7b5d37f6e9003ba0f3ed2efae9c0501a6be63c9
Philippe Vienne <philippe.vienne@insa-lyon.fr>
2016-03-31 19:21:31 +0200

    Edit routes due to conflict with rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,8 @@ Rails.application.routes.draw do
     unauthenticated :user do
       root :to => 'users/sessions#new', as: :unauthenticated_root
     end
-    get 'admin/yurplan' => 'yurplan_link#index'
-    post 'admin/yurplan' => 'yurplan_link#load'
+    get 'yurplan' => 'yurplan_link#index'
+    post 'yurplan' => 'yurplan_link#load'
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
